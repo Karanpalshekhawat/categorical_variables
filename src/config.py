b5ca68af584bed8bb76738424174f3cbb41ec01c
karanpalsinghshekhawat <karanpal609@gmail.com>
2020-10-30 10:39:44 +0530

    model output added and input file removed

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ This script is used to initialise the important
 static variables
 """
 
-TRAINING_FILE = './input/train.csv'
+TRAINING_FILE = r'/Users/karanpalsinghshekhawat/Desktop/ML-Abhishek_Thakur/train.csv'
 OUTPUT_FILE = './models/'
